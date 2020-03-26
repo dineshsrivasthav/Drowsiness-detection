d70d3c4923b9676311f2e54c2ceeddabb19585ca
dineshsrivasthav <dineshsrivasthav9@gmail.com>
2020-03-26 16:17:20 +0530

    commit

--- drowsiness detection.py
+++ drowsiness detection.py
@@ -13,7 +13,7 @@ reye = cv2.CascadeClassifier('haar cascade files\haarcascade_righteye_2splits.xm
 
 lbl=['Close','Open']
 
-model = load_model('models/cnncat2.h5')
+model = load_model('models/cnn.h5')
 path = os.getcwd()
 cap = cv2.VideoCapture(0)
 font = cv2.FONT_HERSHEY_COMPLEX_SMALL
