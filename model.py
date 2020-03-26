298fbd749809337d3cbbe8416509bedf58b9d013
dineshsrivasthav <dineshsrivasthav9@gmail.com>
2020-03-26 16:19:23 +0530

    Update model.py

--- model.py
+++ model.py
@@ -39,4 +39,4 @@ model = Sequential([
 
 model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
 model.fit_generator(train_batch, validation_data=valid_batch,epochs=15,steps_per_epoch=SPE ,validation_steps=VS)
-model.save('models/cnnCat2.h5', overwrite=True)
+model.save('models/cnn.h5', overwrite=True)
